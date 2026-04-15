22f4559f554dfdd0ea2815623024cefa6b102017
ultrablob <neelgillshah@gmail.com>
2026-04-14 21:07:48 -0400

    start adding physics

--- src/lib/physics.ts
+++ src/lib/physics.ts
@@ -6,40 +6,23 @@ const Engine = Matter.Engine,
     Composite = Matter.Composite;
 
 // create an engine
-const engine = Engine.create();
+export const engine = Engine.create();
 
-const boxAElement = document.getElementById("boxA") as HTMLDivElement;
-const boxBElement = document.getElementById("boxB") as HTMLDivElement;
+export class Box {
+    element: HTMLDivElement;
+    body: Matter.Body;
 
-// create two boxes and a ground
-const boxA = Bodies.rectangle(400, 200, boxAElement.offsetWidth, boxAElement.offsetHeight);
-const boxB = Bodies.rectangle(450, 50, boxBElement.offsetWidth, boxBElement.offsetHeight);
-const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    constructor(element: HTMLDivElement) {
+        this.element = element;
+        this.element.style.position = "absolute";
+        const rect = element.getBoundingClientRect();
+        this.body = Bodies.rectangle(rect.left + rect.width / 2, rect.top + rect.height / 2, rect.width, rect.height);
+        Composite.add(engine.world, this.body);
+    }
 
-// add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
-
-
-// Create a main loop
-function mainLoop() {
-    // run the engine
-    Engine.update(engine, 1000 / 60);
-    requestAnimationFrame(mainLoop);
-    
-    // log the position of the boxes
-    console.log(`Box A position: x=${boxA.position.x}, y=${boxA.position.y}`);
-    console.log(`Box B position: x=${boxB.position.x}, y=${boxB.position.y}`);
-
-    // update the position of the boxes
-    boxAElement.style.left = `${boxA.position.x - 40}px`;
-    boxAElement.style.top = `${boxA.position.y - 40}px`;
-
-    boxBElement.style.left = `${boxB.position.x - 40}px`;
-    boxBElement.style.top = `${boxB.position.y - 40}px`;
-
-    boxAElement.style.transform = `rotate(${boxA.angle}rad)`;
-    boxBElement.style.transform = `rotate(${boxB.angle}rad)`;
+    update() {
+        this.element.style.left = `${this.body.position.x - 40}px`;
+        this.element.style.top = `${this.body.position.y - 40}px`;
+        this.element.style.transform = `rotate(${this.body.angle}rad)`;
+    }
 }
-
-// Start the main loop
-mainLoop();
