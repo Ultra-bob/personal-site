e6e2b99f6c23e06170cdc124eff52a479077e1bd
ultrablob <neelgillshah@gmail.com>
2026-04-19 13:16:35 -0400

    reduce request waterfall

--- src/lib/physics.ts
+++ src/lib/physics.ts
@@ -518,8 +518,3 @@ export function destroy() {
     mouseConstraint = null;
   }
 }
-
-const footer = document.querySelector("footer");
-if (footer) footer.classList.add("kinematic-box");
-debugger;
-init(); // Start the physics initialization process
